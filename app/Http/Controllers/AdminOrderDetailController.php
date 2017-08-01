563fcfa26455ea114365c1d345b6dd7ee197ec5b
Duy LC <duylc145@gmail.com>
2017-08-01 15:22:09 +0700

    edit list product

--- app/Http/Controllers/AdminOrderDetailController.php
+++ app/Http/Controllers/AdminOrderDetailController.php
@@ -11,9 +11,10 @@ class AdminOrderDetailController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id)
     {
-        $orderdetails = OrderDetail::all();
+        $orderdetails = OrderDetail::where('order_id', '=', $id)->get();
+        // dd($orderdetails);
         return view('auth.orderdetail.index')->with('orderdetails', $orderdetails);
     }
 
